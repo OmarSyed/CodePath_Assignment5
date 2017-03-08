24f04dc4b95d88ae782f6bcf1cb470fcbf827ef8
OmarSyed <omarsyed26@gmail.com>
2017-03-07 19:35:16 -0500

    Update crypto_functions.php

--- apex/private/crypto_functions.php
+++ apex/private/crypto_functions.php
@@ -6,11 +6,22 @@
 const CIPHER_METHOD = 'AES-256-CBC';
 
 function key_encrypt($string, $key, $cipher_method=CIPHER_METHOD) {
-  return "D4RK SH4D0W RUL3Z";
+  $key = str_pad($key, 32, '*'); 
+  $iv_length = openssl_cipher_iv_length($cipher_method); 
+  $iv = openssl_random_pseudo_bytes($iv_length);
+  $encrypted = openssl_encrypt($string, $cipher_method, $key, OPENSSL_RAW_DATA, $iv);
+  $message = $iv . $encrypted; 
+  echo base64_encode($message);
 }
 
 function key_decrypt($string, $key, $cipher_method=CIPHER_METHOD) {
-  return "PWNED YOU!";
+	$key = str_pad($key, 32, '*');
+	$iv_with_ciphertext = base64_decode($string);
+	$iv_length = openssl_cipher_iv_length($cipher_method);
+	$iv = substr($iv_with_ciphertext, 0, $iv_length);
+	$ciphertext = substr($iv_with_ciphertext, $iv_length);
+	$plaintext = openssl_decrypt($ciphertext, CIPHER_METHOD, $key, OPENSSL_RAW_DATA, $iv);
+	echo $plaintext;
 }
 
 
